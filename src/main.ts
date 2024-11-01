14fe1f60225aba235b6bbade75c6356197d2824b
Pro-shanto06 <proshantosaha1999@gmail.com>
2024-11-01 20:22:56 +0600

    add signin with google

--- src/main.ts
+++ src/main.ts
@@ -2,11 +2,14 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { Logger, ValidationPipe } from '@nestjs/common';
 import { appConfig } from './configuration/app.config';
+import { CorsMiddleware } from './middleware/cors.middleware';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
+  app.use(new CorsMiddleware().use);
   app.useGlobalPipes(new ValidationPipe());
+
   const logger = new Logger('Bootstrap');
   await app.listen(appConfig.port);
   logger.log(`Application is running on: http://localhost:${appConfig.port}`);
---
+++ src/middleware/cors.middleware.ts
@@ -0,0 +1,18 @@
+import { Injectable, NestMiddleware } from '@nestjs/common';
+import { Request, Response, NextFunction } from 'express';
+
+@Injectable()
+export class CorsMiddleware implements NestMiddleware {
+  use(req: Request, res: Response, next: NextFunction) {
+    res.setHeader('Access-Control-Allow-Origin', 'http://localhost:5173');
+    res.setHeader('Access-Control-Allow-Methods', 'GET,POST,PUT,DELETE,OPTIONS');
+    res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+    res.setHeader('Access-Control-Allow-Credentials', 'true');
+
+    if (req.method === 'OPTIONS') {
+      return res.sendStatus(204);
+    }
+
+    next();
+  }
+}
